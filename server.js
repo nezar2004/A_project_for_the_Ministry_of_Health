19a8536f9c8220f5de05fbabacfea5c7d3c45231
Nezar Ahmad <nezarmoon2004@gmail.com>
2025-11-24 13:28:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GOOGLE_API_KEY
-});
+const ai = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
 app.post("/api/chat", async (req, res) => {
-  const question = req.body.question;
   try {
-    const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash-lite",
-      contents: [{ text: question }],
-    });
-    const answer = response.candidates[0].content.parts[0].text;
+    const question = req.body.question;
+
+    const model = ai.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+    const result = await model.generateContent(question);
+
+    const answer = result.response.text();
+
     res.json({ answer });
   } catch (e) {
     console.error("AI ERROR:", e);
-    res.json({ answer: "خطأ في الاتصال بالسيرفر." });
+    res.json({ answer: "خطأ في الاتصال بالسيرفر" });
   }
 });
 
