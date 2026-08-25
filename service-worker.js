bb2b8975be0e7e9130458b11e66cecb9aa3e0c78
Nezar Ahmad <nezarmoon2004@gmail.com>
2026-08-25 19:38:35 +0300

    Improve security, mobile layout, and PWA support

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "amal-shell-v2";
+const SHELL_FILES = [
+  "./",
+  "./index.html",
+  "./main-style.css",
+  "./login_Patient.html",
+  "./doctor_login.html",
+  "./login_style.css",
+  "./Patient_booking_page.html",
+  "./booking-style.css",
+  "./doctor_dashboard.html",
+  "./dashboard-style.css"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
